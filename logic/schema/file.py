3301e9b80deb741f547f104d654c536a55d31196
Aditya Narayan Rai <raiadi2008@gmail.com>
2025-03-28 23:10:17 +0530

    file request schemas

--- logic/schema/file.py
+++ logic/schema/file.py
@@ -4,10 +4,36 @@ from uuid import UUID
 
 class FileStore(BaseModel):
     """
-    File Store Schema
+    File Store Schema corresponding to sqlalchemy
     """
 
     id: UUID
     file_path: str
     file_name: str
     projects: list = Field(default_factory=list)
+
+
+class FileUploadRequest(BaseModel):
+    """
+    File upload request json schema
+    """
+
+    file_paths: list[str]
+    project_id: UUID
+
+
+class FileDeleteRequest(BaseModel):
+    """
+    Delete file request json schema
+    """
+
+    file_ids: list[UUID]
+
+
+class FileUpdateRequest(BaseModel):
+    """
+    File update request json schema
+    """
+
+    file_path: str
+    file_id: UUID
