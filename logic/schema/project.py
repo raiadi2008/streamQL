27523293be84ad60606d2d7a036639c27520d06a
Aditya Narayan Rai <raiadi2008@gmail.com>
2025-04-03 13:10:17 +0530

    logic schema

--- logic/schema/project.py
+++ logic/schema/project.py
@@ -2,10 +2,12 @@ from pydantic import BaseModel, Field
 from typing import Optional
 from uuid import UUID
 
+from logic.schema.file import FileStore as FileStoreSchema
+
 
 class ProjectStore(BaseModel):
     """
-    Project Store Schema
+    Project Store Schema corresponding to sql alchemy
     """
 
     id: UUID
@@ -13,3 +15,17 @@ class ProjectStore(BaseModel):
     project_description: Optional[str] = Field(default=None)
     project_db_name: UUID
     files: list = Field(default_factory=list)
+
+
+class CreateProjectRequest(BaseModel):
+    project_name: str
+    description: Optional[str] = None
+
+
+class UpdateProjectRequest(BaseModel):
+    files: Optional[list[FileStoreSchema]] = Field(default_factory=list)
+    description: Optional[str] = None
+
+
+class FileOpsRequest(BaseModel):
+    files: list[FileStoreSchema]
